a2a157f1f4c21e507fd2a982966253bf8768ed59
Peter <github@pixeldepth.net>
2017-02-09 01:25:58 +0000

    RNG

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LCG-RNG
-Seeded random number
+https://en.wikipedia.org/wiki/Linear_congruential_generator
+
+Needed a very basic way to seed a random number.
+
+let rng = new RNG(100);
+
+rng.next(); // 0.38643872755879477
---
+++ RNG.js
@@ -0,0 +1,16 @@
+class RNG {
+
+	constructor(seed){
+		this.m = 2147483647;
+		this.a = 1103515245;
+		this.c = 12345;
+		this.seed = seed || Math.floor(Math.random() * this.m);
+	}
+
+	next(){
+		this.seed = (this.a * this.seed + this.c) % this.m;
+
+		return this.seed / this.m;
+	}
+
+}
